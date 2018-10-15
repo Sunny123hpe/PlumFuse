868c61c03282315d0d9795f68acfe4182abfaea3
Shashank-23 <35955533+Shashank-23@users.noreply.github.com>
2018-10-15 20:52:12 +0530

    FIrstCommit

--- src/main/java/CreateAnFuseApp/Project/TestCase1.java
+++ src/main/java/CreateAnFuseApp/Project/TestCase1.java
@@ -5,51 +5,50 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.pom.CreateAnApp;
-import com.pom.KnwBase;
+
 
 import selenium.pom.BaseTest1;
 
 public class TestCase1 extends BaseTest1{
-	
+
+@Parameters({"Pagename","Phoneno","MessageSendasms","SMTP","PortNo","Username","Pwd","From","To","Subject","MailMessage"})	
 @Test
-@Parameters({ "Pagename" },{"Phoneno"})
-public void CreateanApp(@Optional("Abc") String Pagename) throws InterruptedException
+public void CreateanApp(String page,String Phoneno,String MessageSendasms,String SMTP,String PortNo,String Username,String Pwd,String From,String To,String Subject,String MailMessage) throws InterruptedException
 {
 	
 	CreateAnApp app=new CreateAnApp(get());
-	List<WebElement> iframeElements = get().findElements(By.tagName("iframe"));
-	System.out.println("Total Iframe are "+iframeElements.size());
 	app.clickonCreateanApp();
 	app.clickonLetsGetStarted();
 	app.clickonNewPage();
-	app.Enteranewname(Pagename);
+	app.Enteranewname(page);
 	app.ClickonCreate();
 	app.clickonMessagingdropdown();
 	app.clickonAddMessaging();
-	app.Enterphoneno("121638383");
-	app.EnterTextInMessageText("Hello World");
+	app.Enterphoneno(Phoneno);
+	app.EnterTextInMessageText(MessageSendasms);
 	app.clickonAddEmail();
-	app.EnterTextInSMTPHost("smtp@gmail.com");
-	app.EnterTextInPort("465");
-	app.EnterTextInUsername("abc@gmail.com");
-	app.EnterTextInPassword("1234");
-	app.EnterTextInFrom("abc@gmail.com");
-	app.EnterTextInTo("xyz@gmail.com");
-	app.EnterTextInSubject("SmsnotSent");
-	app.EnterTextInMessageTextinEamil("Sms to phone no 121638383 not sent");
+	app.EnterTextInSMTPHost(SMTP);
+	app.EnterTextInPort(PortNo);
+	app.EnterTextInUsername(Username);
+	app.EnterTextInPassword(Pwd);
+	app.EnterTextInFrom(From);
+	app.EnterTextInTo(To);
+	app.EnterTextInSubject(Subject);
+	app.EnterTextInMessageTextinEamil(MailMessage);
 	app.DragtheMailWindow(300, 100);
 	app.ClickonBasicDropDown();
 	app.ClickonExit();
-	Thread.sleep(5000);
+	Thread.sleep(3000);
 	app.ClickonExit();
-	Thread.sleep(5000);
+	Thread.sleep(3000);
 	app.ClickonExit();
-	Thread.sleep(5000);
+	Thread.sleep(3000);
 	app.DragThirdExitWnd(600, 160);
 	app.DragSecondExitWnd(90, 100);
 	app.DragfirstExitWnd(5, 100);
@@ -58,5 +57,6 @@ public void CreateanApp(@Optional("Abc") String Pagename) throws InterruptedExce
 	app.jointheRightsmsnodeandEmailNode();
 	app.JointhleftEamilandSecondexitnode();
 	app.JointhRightEamilandThirdexitnode();	
+	
 }
 }
--- src/main/java/selenium/pom/BaseTest1.java
+++ src/main/java/selenium/pom/BaseTest1.java
@@ -13,7 +13,7 @@ public class BaseTest1 extends SeleniumUtility
 	@BeforeMethod
 	
 	public void setUp() throws Exception
-	{   System.setProperty("webdriver.chrome.driver", "C:\\jar\\chromedriver.exe");
+	{   System.setProperty("webdriver.chrome.driver", "./Test_Library/chromedriver.exe");
 	set(new ChromeDriver());
 	get().manage().window().maximize();
 	get().get("http://quickfuseapps.com");
